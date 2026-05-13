8f787911744fecd1cee9069740995e0059c28464
levmarc19-svg <levmarc19@ecolecatholique.ca>
2026-05-13 19:06:16 -0400

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,11 +1,10 @@
-function handlePlay() {
-  const username = document.getElementById('txtusername-input').value.trim();
+ const username = document.getElementById('txtusername-input').value.trim();
   if (username === "") {
     alert("S'il vous plaît, entrez un nom d'utilisateur !");
     return;
   }
   localStorage.setItem('mathAttaqueUser', username);
   window.location.href = "game.html";
-}
+
 
  
